53b619c4ac799f1f6e4f0213465c0c6ec0356766
adnanhashmi09 <hashmi.adnan791@gmail.com>
2022-12-02 15:59:36 +0530

    maped jk to ESC

--- lua/maps.lua
+++ lua/maps.lua
@@ -23,3 +23,6 @@ map("n", "<C-Up>", ":resize -2<CR>")
 map("n", "<C-Down>", ":resize +2<CR>")
 map("n", "<C-Right>", ":vertical resize -2<CR>")
 map("n", "<C-Left>", ":vertical resize +2<CR>")
+
+-- Press jk fast to enter
+map("i", "jk", "<ESC>")
